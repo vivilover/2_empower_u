c39f04f667ca006b3281c1fcb10c57d76b9e4bd4
Nelson Cheng-Lin <nelsonchenglin@gmail.com>
2023-08-17 17:23:13 -0400

    testing main branch protection rules

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ function App() {
   return (
     <div className="w-screen h-screen bg-gray-500">
       <p className="text-2xl">Hello world</p>
+      <p> testing main branch protection</p>
     </div>
   )
 }
