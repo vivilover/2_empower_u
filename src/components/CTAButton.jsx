d9b360761eb6ba9de76caa9abc111b67546cc4e2
Shin Lee <jakelee1204@gmail.com>
2023-08-17 16:16:05 -0700

    Header and CallToAction components groundwork

---
+++ src/components/CTAButton.jsx
@@ -0,0 +1,9 @@
+
+export default function CTAButton({ text, btnId }) {
+  return (
+    <div className="flex justify-start items-center bg-white w-3/5 rounded-xl h-12 gap-x-5">
+      <div className="w-8 h-8 ml-16 bg-slate-400 rounded-full flex items-center justify-center">{btnId}</div>
+      <div className="">{text}</div>
+    </div>
+  )
+}
