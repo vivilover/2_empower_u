3aee140f7e735b0e9c56c7c4d99844b40d17bd42
Shin Lee <jakelee1204@gmail.com>
2023-08-19 14:59:52 -0700

    Created SocialMedia follow section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Header from './components/Header.jsx';
 import CallToAction from './components/CallToAction.jsx';
+import SocialMedia from './components/SocialMedia.jsx';
+import Testimonials from './components/Testimonials.jsx';
 
 function App() {
 
@@ -7,6 +9,8 @@ function App() {
     <div className="w-screen h-screen">
       <Header />
       <CallToAction />
+      <SocialMedia />
+      <Testimonials />
     </div>
   )
 }
